983c42121e82502af6fb89d0d82f37b4d3c2b8eb
mei <meike.pollakowski@gmail.com>
2021-01-24 16:23:04 +0100

    Add form validation and change post views

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -16,6 +16,8 @@ SET SESSION AUTHORIZATION DEFAULT;
 
 ALTER TABLE public.posts DISABLE TRIGGER ALL;
 
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('5ef326de-1a3f-4fd0-b077-10607196aa5b', 'first post', 'hi', '2021-01-24 12:05:57.224048+01');
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('cf49d931-81e0-4579-bd7e-fca433c075e9', 'second post', 'hallo', '2021-01-24 12:06:03.355872+01');
 
 
 ALTER TABLE public.posts ENABLE TRIGGER ALL;
--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -11,6 +11,7 @@ instance View ShowView where
                 <li class="breadcrumb-item active">Show Post</li>
             </ol>
         </nav>
-        <h1>Show Post</h1>
-        <p>{post}</p>
+        <h1>{ get #title post}</h1>
+        <p>{ get #createdAt post |> timeAgo}</p>
+        <p>{ get #body post}</p>
     |]
