b4cc8a41adec172a5081fc082358244583cea577
mei <meike.pollakowski@gmail.com>
2021-01-24 17:59:28 +0100

    Add comments table and generate controller

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -16,10 +16,18 @@ SET SESSION AUTHORIZATION DEFAULT;
 
 ALTER TABLE public.posts DISABLE TRIGGER ALL;
 
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('cf49d931-81e0-4579-bd7e-fca433c075e9', 'second post', '# hey
+**some bold text**', '2021-01-24 12:06:03.355872+01');
 INSERT INTO public.posts (id, title, body, created_at) VALUES ('5ef326de-1a3f-4fd0-b077-10607196aa5b', 'first post', 'hi', '2021-01-24 12:05:57.224048+01');
-INSERT INTO public.posts (id, title, body, created_at) VALUES ('cf49d931-81e0-4579-bd7e-fca433c075e9', 'second post', 'hallo', '2021-01-24 12:06:03.355872+01');
 
 
 ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.comments DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.comments ENABLE TRIGGER ALL;
 
 
